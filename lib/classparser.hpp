5964b7f293ad9aabeb82e9f958dce2c89c8f262b
julienlopez <julien.lopez51@gmail.com>
2019-12-10 15:15:57 +0100

    extracting class info into its own file

--- lib/classparser.hpp
+++ lib/classparser.hpp
@@ -2,57 +2,10 @@
 
 #include <optional>
 
-#include "file.hpp"
+#include "class.hpp"
 
 namespace Cda
 {
-
-struct ClassFiles
-{
-    ClassFiles(File header_file_, std::optional<File> source_file_ = std::nullopt);
-
-    File header_file;
-    std::optional<File> source_file;
-};
-
-enum class Visibility
-{
-    Private,
-    Protected,
-    Public
-};
-
-struct MemberFunction
-{
-    Visibility visibility;
-    // std::wstring return_type;
-    std::wstring name;
-    bool is_const;
-};
-
-struct MemberVariable
-{
-    Visibility visibility;
-    std::wstring type;
-    std::wstring name;
-    bool is_reference;
-    bool is_const;
-};
-
-struct HeaderContent
-{
-    std::vector<MemberFunction> functions;
-    std::vector<MemberVariable> variables;
-};
-
-struct Class
-{
-    Class(std::wstring name_, ClassFiles files_, HeaderContent header_content_);
-
-    const std::wstring name;
-    ClassFiles files;
-    HeaderContent header_content;
-};
 
 class ClassParser
 {
